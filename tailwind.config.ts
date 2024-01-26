513e3b93f99c673fe50697ed752b8271ca22d714
Sagar Siwach <sagarsiwach19@outlook.com>
2024-01-26 11:42:12 +0530

    Navigation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,6 +19,34 @@ const config = {
     },
     extend: {
       colors: {
+        flamingo: {
+          "50": "#fef5ee",
+          "100": "#fde8d7",
+          "200": "#fbccad",
+          "300": "#f8a879",
+          "400": "#f47a43",
+          "500": "#f15a22",
+          "600": "#e23e14",
+          "700": "#bb2d13",
+          "800": "#952417",
+          "900": "#782116",
+          "950": "#410d09",
+        },
+
+        ecstasy: {
+          "50": "#fff9ec",
+          "100": "#fff2d3",
+          "200": "#ffe1a5",
+          "300": "#ffcb6d",
+          "400": "#ffa932",
+          "500": "#ff8e0a",
+          "600": "#ff7500",
+          "700": "#cc5502",
+          "800": "#a1410b",
+          "900": "#82380c",
+          "950": "#461a04",
+        },
+
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -74,11 +102,11 @@ const config = {
       },
     },
     fontFamily: {
-      sans: ['var(--font-geist-sans)', 'sans-serif'], // Geist Variable for sans-serif
-      mono: ['var(--font-jetbrains-mono)', 'monospace'], // JetBrains Mono for monospace
+      sans: ["var(--font-geist-sans)", "sans-serif"], // Geist Variable for sans-serif
+      mono: ["var(--font-jetbrains-mono)", "monospace"], // JetBrains Mono for monospace
+    },
   },
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require("tailwindcss-animate"), "prettier-plugin-tailwindcss"],
+} satisfies Config;
 
-export default config
+export default config;

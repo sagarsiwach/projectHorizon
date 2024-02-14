4b6672c11ab918540b9ad60cae2b1db1a1027d06
Sagar Siwach <sagarsiwach19@outlook.com>
2024-02-14 15:35:36 +0530

    Final

--- app/_km75/page.tsx
+++
@@ -1,6 +0,0 @@
-import Navigation from "@/components/navigation/Navigation";
-import Ribbon from "@/components/navigation/Ribbon";
-
-export default function HomePage() {
-  return <div>Test Ride</div>;
-}
--- app/km5000/page.tsx
+++
@@ -1,9 +0,0 @@
-import Navigation from "@/components/navigation/Navigation";
-import Ribbon from "@/components/navigation/Ribbon";
-import { Button } from "@/components/ui/button";
-import { EnvelopeClosedIcon } from "@radix-ui/react-icons";
-import { DownloadCloud } from "lucide-react";
-
-export default function HomePage() {
-  return <div>KM5000</div>;
-}
